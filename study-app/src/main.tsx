db08d05d6e39bde7ac256172370f1ea0e076710f
smitha-sheshadri <smitha3shesh@gmail.com>
2026-08-24 17:29:56 +0800

    Refactor imports and App component syntax
    Updated import statements to use double quotes and fixed App component syntax.

--- study-app/src/main.tsx
+++ study-app/src/main.tsx
@@ -1,14 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-import outputs from '../amplify_outputs.json';
-import App from './App';
-import './styles.css';
-
-Amplify.configure(outputs);
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App/>
+    <App />
   </React.StrictMode>
 );
