2161a8bc4aaddea13e96199654deb5f79f0ed648
smitha-sheshadri <smitha3shesh@gmail.com>
2026-08-24 17:27:01 +0800

    Remove unused Amplify import from main.tsx

--- study-app/src/main.tsx
+++ study-app/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {Amplify} from 'aws-amplify';
+
 import outputs from '../amplify_outputs.json';
 import App from './App';
 import './styles.css';
